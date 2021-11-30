368dc631d5d910f4e38d52ea34b8760686731889
aniket312001 <70058567+aniket312001@users.noreply.github.com>
2021-11-30 13:54:44 +0530

    Add files via upload

---
+++ node4.js
@@ -0,0 +1,19 @@
+
+//   Serving Html Page
+
+
+
+
+const http = require("http")
+const fs = require("fs")   //including file system module
+const fileContent = fs.readFileSync("a.html",'utf-8')
+
+const server = http.createServer((req,res)=>{  
+    
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end(fileContent)
+});
+
+server.listen(80, '127.0.0.1', ()=>{
+    console.log("listening on port 80");
+});
